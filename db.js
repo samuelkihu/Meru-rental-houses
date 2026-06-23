e3f13054424adc2e1b8abcc32a2284d06fbec867
samuel kihu <kihusamuel935@gmail.com>
2026-06-24 01:18:32 +0300

    Fix database pool configuration for MySQL

--- db.js
+++ db.js
@@ -8,8 +8,7 @@ const pool = mysql2.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-    database: process.env.DB_PORT,
-
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool.promise();
